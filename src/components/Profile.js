f211ee027c29269e5592137202008dbd6147e02b
p106305 <46699595+p106305@users.noreply.github.com>
2022-01-11 03:52:33 +0900

    add homepage

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,9 @@
-import { React } from "react";
+import { React, useState } from "react";
+import { Loading } from "../components";
 
 function Profile({id, className, profile, name, to, children}) {
+    const [loading, setLoading] = useState(true);
+
     return (
         <section id={id} className={`profile ${className}`}>
             <div id="info" className="center">
@@ -9,7 +12,9 @@ function Profile({id, className, profile, name, to, children}) {
                         className="circle"
                         src={profile}
                         alt={children}
+                        onLoad={() => setLoading(false)}
                     ></img>
+                    <div style={{display: loading ? "block" : "none"}}><Loading /></div>
                 </a>
                 <h1>{name}</h1>
                 <a className="shortcut" href={to} target="_blank" rel="noopener noreferrer" >{`${children} 바로가기`}</a>
